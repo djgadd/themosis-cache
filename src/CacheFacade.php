bbc25f3047cb7f3ab0a9fd56d748129bf5ccea45
Daniel Gadd <DJGadd@DJGadd.local>
2017-04-11 15:26:18 +0100

    * Updating class name to match filename for psr4

--- src/CacheFacade.php
+++ src/CacheFacade.php
@@ -4,7 +4,7 @@ namespace KeltieCochrane\Cache;
 
 use Themosis\Facades\Facade;
 
-class Cache extends Facade
+class CacheFacade extends Facade
 {
   /**
    * Get the registered name of the component.
