28ee8e9113ba48b65f059b52a14035605248e107
Daniel Gadd <DJGadd@DJGadd.local>
2017-04-18 17:04:13 +0100

    * Defer loading the cache service

--- src/CacheServiceProvider.php
+++ src/CacheServiceProvider.php
@@ -10,6 +10,14 @@ use KeltieCochrane\Cache\Drivers\WordPressStore;
 class CacheServiceProvider extends ServiceProvider
 {
   /**
+   * Defer loading unless we need it, saves us a little bit of overhead if the
+   * current request isn't trying to log anything.
+   *
+   * @var bool
+   */
+  protected $defer = true;
+
+  /**
    * Perform post-registration booting of services.
    * @return void
   **/
